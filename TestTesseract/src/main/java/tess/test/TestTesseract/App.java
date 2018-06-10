cb0ce662181da2246e8a69bc4a94197658fab31e
Rahul Vaish <vaish.rahul4810@gmail.com>
2018-06-10 23:44:33 +0530

    Add files via upload

---
+++ TestTesseract/src/main/java/tess/test/TestTesseract/App.java
@@ -0,0 +1,13 @@
+package tess.test.TestTesseract;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
